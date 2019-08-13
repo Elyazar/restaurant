2d9e1ae3836da831b4f25c907b5f94c110a53ad9
Lazar Yasinskiy <lazar1999yasinskiy@gmail.com>
2019-08-13 11:44:15 +0300

    Сверстал адаптивное меню, исправил мелкие ошибки

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ $(function(){
       dots: true,
       autoplay: true,
       autoplaySpeed: 3000,
+      cssEase: 'cubic-bezier(0,0,1,1.01)',
       adaptiveHeight: true,
       arrows: false,
   });
